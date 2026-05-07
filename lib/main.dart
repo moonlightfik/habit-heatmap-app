859e29dbb2a56dd14fdeb3108c84095928290a73
Eyobketemaw <jobeket@gmail.com>
2026-05-07 14:13:31 +0300

    update flutter project and dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:habit_heatmap/core/theme/app_theme.dart';
 import 'package:habit_heatmap/models/habit.dart';
 import 'package:habit_heatmap/providers/habit_provider.dart';
 import 'package:habit_heatmap/providers/theme_provider.dart';
-import 'package:habit_heatmap/services/notification_service.dart';
 import 'package:provider/provider.dart';
 import 'package:habit_heatmap/routes/app_routes.dart';
 
 /// Global navigator key used by NotificationService to show dialogs
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData.light();
+  static final ThemeData darkTheme = ThemeData.dark();
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   // Initialize Hive
   await Hive.initFlutter();
-  
+
   // Register adapters
   Hive.registerAdapter(HabitAdapter());
   Hive.registerAdapter(HabitEntryAdapter());
   Hive.registerAdapter(HabitFrequencyAdapter());
-  
+
   // Open boxes
   await Hive.openBox<Habit>('habits');
-  
+
   runApp(const MyApp());
 }
 
