c90cfe64e73cdf6ec3e92ebe1deea4821898028a
ebu0g <gebawak@gmail.com>
2026-05-08 13:48:33 +0300

    Add files via upload

---
+++ lib/models/habit.g.dart
@@ -0,0 +1,147 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'habit.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class HabitAdapter extends TypeAdapter<Habit> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Habit read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Habit._internal(
+      id: fields[0] as String,
+      name: fields[1] as String,
+      frequency: fields[2] as HabitFrequency,
+      colorValue: fields[3] as int,
+      dailyGoal: fields[4] as int,
+      enableReminders: fields[5] as bool,
+      reminderTimeHour: fields[6] as int?,
+      reminderTimeMinute: fields[7] as int?,
+      createdAt: fields[8] as DateTime,
+      entries: (fields[9] as List?)?.cast<HabitEntry>(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Habit obj) {
+    writer
+      ..writeByte(10)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.name)
+      ..writeByte(2)
+      ..write(obj.frequency)
+      ..writeByte(3)
+      ..write(obj.colorValue)
+      ..writeByte(4)
+      ..write(obj.dailyGoal)
+      ..writeByte(5)
+      ..write(obj.enableReminders)
+      ..writeByte(6)
+      ..write(obj.reminderTimeHour)
+      ..writeByte(7)
+      ..write(obj.reminderTimeMinute)
+      ..writeByte(8)
+      ..write(obj.createdAt)
+      ..writeByte(9)
+      ..write(obj.entries);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is HabitAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class HabitEntryAdapter extends TypeAdapter<HabitEntry> {
+  @override
+  final int typeId = 1;
+
+  @override
+  HabitEntry read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return HabitEntry(
+      habitId: fields[0] as String,
+      date: fields[1] as DateTime,
+      progress: fields[2] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, HabitEntry obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.habitId)
+      ..writeByte(1)
+      ..write(obj.date)
+      ..writeByte(2)
+      ..write(obj.progress);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is HabitEntryAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+class HabitFrequencyAdapter extends TypeAdapter<HabitFrequency> {
+  @override
+  final int typeId = 2;
+
+  @override
+  HabitFrequency read(BinaryReader reader) {
+    switch (reader.readByte()) {
+      case 0:
+        return HabitFrequency.daily;
+      case 1:
+        return HabitFrequency.weekly;
+      default:
+        return HabitFrequency.daily;
+    }
+  }
+
+  @override
+  void write(BinaryWriter writer, HabitFrequency obj) {
+    switch (obj) {
+      case HabitFrequency.daily:
+        writer.writeByte(0);
+        break;
+      case HabitFrequency.weekly:
+        writer.writeByte(1);
+        break;
+    }
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is HabitFrequencyAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
